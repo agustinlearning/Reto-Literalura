82e9974eac0dee0340f6c8d36b9c22c4911c3d1f
agustin <agustinalvarez7378@gmail.com>
2025-06-06 09:19:26 -0400

    create project

---
+++ Literalura/src/main/java/com/Aluracursos/Literalura/LiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.Aluracursos.Literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+}
