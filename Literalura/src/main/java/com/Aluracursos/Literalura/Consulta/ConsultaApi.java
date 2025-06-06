4a27d423485beb6b45217cb35bffd6db9b8d9d4e
agustin <agustinalvarez7378@gmail.com>
2025-06-06 10:35:46 -0400

    using the api gutendex

--- Literalura/src/main/java/com/Aluracursos/Literalura/Consulta/ConsultaApi.java
+++ Literalura/src/main/java/com/Aluracursos/Literalura/Consulta/ConsultaApi.java
@@ -7,7 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaApi {
-    URI direccion = URI.create("https://gutendex.com/books/?page=");
+    URI direccion = URI.create("https://gutendex.com/books/?title=");
 
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
