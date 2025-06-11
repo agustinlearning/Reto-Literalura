744b09b85aad9b18637fa9a1863294e86f1dd2a5
agustin <agustinalvarez7378@gmail.com>
2025-06-11 06:38:46 -0400

    adding gjsonignoreproperties to record result

--- Literalura/src/main/java/com/Aluracursos/Literalura/LiteraluraApplication.java
+++ Literalura/src/main/java/com/Aluracursos/Literalura/LiteraluraApplication.java
@@ -1,13 +1,11 @@
 package com.Aluracursos.Literalura;
 
-import com.Aluracursos.Literalura.model.Libro;
 import com.Aluracursos.Literalura.service.LibroService;
-import org.springframework.beans.factory.annotation.Autowired; // Importante
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -51,7 +49,7 @@ public class LiteraluraApplication implements CommandLineRunner {
 			System.out.println("1. Buscar libro por título");
 			System.out.println("2. Mostrar libros registrados");
 			System.out.println("3. Mostrar autores registrados");
-			System.out.println("4. Listar libros por idioma ej: en(ingles) es(español)");
+			System.out.println("4. Listar libros por idioma");
 			System.out.println("5. Listar autores vivos en un año determinado");
 			System.out.println("0. Salir");
 			System.out.print("Elija una opción: ");
--- Literalura/src/main/java/com/Aluracursos/Literalura/model/Result.java
+++ Literalura/src/main/java/com/Aluracursos/Literalura/model/Result.java
@@ -1,7 +1,11 @@
 package com.Aluracursos.Literalura.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Result(
         String title,
         List<AutorData> authors,
